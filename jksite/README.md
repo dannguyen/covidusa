f7898a787ed8d6833a46f5fbce0be9f2dfbe0b9f
Dan Nguyen <dansonguyen@gmail.com>
2020-03-24 17:53:36 -0500

    no idea why jekyllsite was a submodule

---
+++ jksite/README.md
@@ -0,0 +1,10 @@
+# COVID USA
+
+
+https://dannguyen.github.io/covidusa/
+
+I like making Jekyll sites that don't do anything fancy beyond tweaking Bootstrap 4.x. So this is a theme that has some basic scaffolding, as well as vendoring a recent version of Bootstrap.
+
+
+**Current Bootstrap version:**  v4.3.1 (https://getbootstrap.com/)
+
---
+++ jksite/_site/assets/js/nationMap.js
@@ -0,0 +1,58 @@
+var svg = d3.select("#mapbox"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+
+// var url = "https://d3js.org/us-10m.v1.json"
+
+var states_url = "/covidusa/static/data/states.topo.json?v=0.0.1a"
+
+
+var us_states = d3.json(states_url);
+var xstates = 1;
+Promise.all([us_states])
+.then(function(the_response){
+    console.log("fetched url", the_response)
+    geodata = the_response[0]
+
+//    var projection = d3.geoAlbers().scale(800) //.fitWidth(width, glob) //.fitWidth(width, gdata) // .fitSize([width, height]);
+    var path = d3.geoPath()
+
+    var states = topojson.feature(geodata, geodata.objects.states).features
+    xstates = states
+
+
+    svg.selectAll("path")
+        .data(states)
+        .enter()
+        .append("path")
+        .attr("class", "state")
+        .attr("d", path)
+        .attr("")
+
+
+    // svg.append("g")
+    //     .attr("class", "states")
+    //     .selectAll("path")
+    //     .data(states)
+    //     .enter()
+    //     .append("path")
+    //     .attr("d", path)
+
+    // svg.selectAll("text")
+    //     .data(states)
+    //     .enter()
+    //     .append("text")
+    //     .text(function(d){
+    //         return "hi"
+    //     })
+    //     // .attr("x", function(d) {return projection([d.Longitude, d.Lattitude])[0] + 5;})
+    //     // .attr("y", function(d) {return projection([d.Longitude, d.Lattitude])[1] + 15;})
+    //     .attr("class", "abbr")
+
+
+})
+.catch(function(error){
+    if (error) throw error;
+
+})
